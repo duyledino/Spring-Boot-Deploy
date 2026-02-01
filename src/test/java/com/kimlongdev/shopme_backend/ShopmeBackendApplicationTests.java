c32a3ea0e547a9bdb04c0a1bf9293e09c735ae30
Tristan Long <trieukimlong102000@gmail.com>
2026-02-01 12:33:13 +0700

    Initial project setup

---
+++ src/test/java/com/kimlongdev/shopme_backend/ShopmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimlongdev.shopme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
